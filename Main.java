0d54d7383a2e0dfc2e24b4fc87552602fcf46281
Leslie Wen <40618577+LeslieWen@users.noreply.github.com>
2020-08-11 16:53:01 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package gchallenge;
-
+import java.util.Scanner;
 /**
  *
  * @author lesliewen
@@ -16,6 +16,10 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        //Controlller
+        
+        
         
         //Challenge 3 - Temperature converter
         /*
